a4e47151c2814fa71024fc6f06ef17f4bb17e27b
Eissa <baawaidhane@gmail.com>
2025-08-31 21:50:00 +0300

    update icon for login page

--- public/ui/authentification/login.ui.php
+++ public/ui/authentification/login.ui.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="Description" content="goldenkit company" />
     <title>180Bit System</title>
+    <link rel="shortcut icon" type="image/x-icon" href="<?= $Icon ?>" />
     <!--<link rel="shortcut icon" type="image/x-icon" href="<?= $Icon ?>" />-->
 
 
